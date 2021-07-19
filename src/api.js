047b1cdd2ac79a85ef9b70fcc3b605023c1ee31a
tony.cho <tony.cho@wavebridgelabs.com>
2021-07-19 09:42:29 +0900

    .

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ const request = (url) => {
     .then(({data}) => data);
 };
 
-const getSymbols = () => request('https://euijincho.github.io/tradingview-test-demo/');
+const getSymbols = () => request('https://euijincho.github.io/tradingview-test-demo/symbols/list.json');
 const getSeries = async (symbol) => {
     const { historical } = await request(`https://euijincho.github.io/tradingview-test-demo/series/${symbol}.json`);
     return historical.map(({ date, adjClose }) => ({ time: date, value: adjClose })).reverse();
